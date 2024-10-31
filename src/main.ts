2dbb6dc5974f1761755b5f6442143ef6bf784f6a
KimMinam06 <minam.kim@merakiplace.kr>
2024-10-31 16:41:26 +0900

    env 설정

--- src/main.ts
+++ src/main.ts
@@ -2,19 +2,27 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
+declare const module: any;
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  const port = process.env.PORT;
+
   const config = new DocumentBuilder()
-    .setTitle('Push Notification')
-    .setDescription(
-      'The API details of the business solution for the Push Notification Demo Application.',
-    )
+    .setTitle('nest practice')
+    .setDescription('my nest practice')
     .setVersion('1.0')
-    .addTag('Notification')
-    .addBearerAuth()
+    .addCookieAuth('connect.sid')
+    .addTag('practice')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  await app.listen(process.env.PORT);
+
+  await app.listen(port);
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 bootstrap();
