cba893cf0944b4452734802adad50920f98326b2
kimminam <bluemind8520@gmail.com>
2024-11-02 21:12:39 +0900

    Delete Melon Controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from 'src/app.controller';
 import { AppService } from './app.service';
-import { MelonController } from './melon/melon.controller';
 import { ConfigModule } from '@nestjs/config'
 import { FirebaseModule } from './Firebase/firebase.module';
 import { MelonModule } from './melon/melon.module';
 import { BugsModule } from './bugs/bugs.module';
 
 @Module({
-  controllers: [AppController, MelonController],
+  controllers: [AppController],
   providers: [AppService],
   imports : [ConfigModule.forRoot({ cache: true }),FirebaseModule, MelonModule, BugsModule]
 })
